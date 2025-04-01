874c41b930c490a932db0d8b73c1f465bb3cbb11
Kassiopeia <kasiananastasiia@gmail.com>
2025-04-01 19:31:37 +0100

    it counts

--- Counter/MainPage.cs
+++ Counter/MainPage.cs
@@ -1,10 +1,26 @@
 namespace Counter;
 
+internal partial record Countable(int Count, int Step)
+{
+    public Countable Increment() => this with
+    {
+        Count = Count + Step
+    };
+}
+
+internal partial record MainModel
+{
+    public IState<Countable> Countable => State.Value(this, () => new Countable(0, 1));
+
+    public ValueTask IncrementCounter()
+        => Countable.UpdateAsync(c => c?.Increment());
+}
+
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this
+        this.DataContext(new MainViewModel(), (page, vm) => page
             .Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
             .Content(new StackPanel()
             .VerticalAlignment(VerticalAlignment.Center)
@@ -17,23 +33,24 @@ public sealed partial class MainPage : Page
                     .Height(150)
                     .Source("ms-appx:///Assets/Images/logo.png"),
 
+                new TextBlock()
+                    .Margin(12)
+                    .HorizontalAlignment(HorizontalAlignment.Center)
+                    .TextAlignment(Microsoft.UI.Xaml.TextAlignment.Center)
+                    .Text(() => vm.Countable.Count, txt => $"Counter: {txt}"),
                 new TextBox()
                     .Margin(12)
                     .HorizontalAlignment(HorizontalAlignment.Center)
                     .TextAlignment(Microsoft.UI.Xaml.TextAlignment.Center)
                     .PlaceholderText("Step Size")
-                    .Text("1"),
-                new TextBlock()
-                    .Margin(12)
-                    .HorizontalAlignment(HorizontalAlignment.Center)
-                    .TextAlignment(Microsoft.UI.Xaml.TextAlignment.Center)
-                    .Text("Counter: 1"),
+                    .Text(x => x.Binding(() => vm.Countable.Step).TwoWay()),
                 new Button()
                     .Margin(12)
                     .HorizontalAlignment(HorizontalAlignment.Center)
+                    .Command(() => vm.IncrementCounter)
                     .Content("Increment Counter by Step Size")
 
-            ));
+            )));
 
     }
 }
